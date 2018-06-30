8fd1c5088ffefbfb2da6ae090c35e984103a55b9
Seirradawson <seirradawson@Seirra-Dawsons-MacBook-Pro.local>
2018-06-29 20:53:26 -0400

    Added or updated the following -> README, node_modules, package.json, config.json, schema.sql, index.js

---
+++ config/connection.js
@@ -0,0 +1,5 @@
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+
+// sequelize (lowercase) references my connection to the DB.
+var sequelize = require("../config/connection.js");
--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,18 @@
+DROP DATABASE IF EXISTS appt_db;
 
-DROP DATABASE IF EXISTS `appt_db`;
-CREATE DATABASE `appt_db`;
-USE `appt_db`;
+CREATE DATABASE appt_db;
+USE appt_db;
 
-CREATE TABLE client (
-    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    client_name VARCHAR(50) NOT NULL,
-    email VARCHAR(50),
-    phone VARCHAR(50)
+
+-- booking info
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  customer_name VARCHAR(75) NOT NULL,
+  email VARCHAR(50),
+  phone VARCHAR(15),
+  customer_id VARCHAR(50),
+  datetime_start datetime NOT NULL,
+  datetime_end datetime NOT NULL,
+  status_id int(11) NOT NULL DEFAULT '1',
+  note VARCHAR(200)
 );
